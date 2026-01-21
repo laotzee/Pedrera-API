c131808f8b73d2cb0dc4fc71f69f1b62131a8a59
laotzee <laotzeabreu@proton.me>
2026-01-21 16:33:26 -0400

    Add flask migrate to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .models.models import db
+from .models.models import db, migrate
 from .routes.routes import api_blueprint
 
 def create_app(app_env):
@@ -8,8 +8,6 @@ def create_app(app_env):
     app.config.from_object(app_env[1])
     app.register_blueprint(api_blueprint)
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
+    migrate.init_app(app, db)
 
     return app
