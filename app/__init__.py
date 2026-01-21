063f4e931453aa5ef419f906421b72a221866c94
laotzee <laotzeabreu@proton.me>
2026-01-21 12:52:58 -0400

    Finish app_create initiator

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,13 @@
 from flask import Flask
 from .models import db
-from .routes import init_routes
-from config import app_env
+from .routes.routes import api_blueprint
 
-def create_app():
+def create_app(app_env):
 
     app = Flask(__name__)
     app.config.from_object(app_env[1])
+    app.register_blueprint(api_blueprint)
     db.init_app(app)
-    init_routes(app)
-
-    print('-----------------------------------------')
-    print(f'Environment configured for {app_env[0]}')
-    print('_________________________________________')
 
     with app.app_context():
         db.create_all()
