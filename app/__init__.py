a21ce13eff6914181d198d155820b2a183fd5235
laotzee <laotzeabreu@proton.me>
2026-01-21 20:01:03 -0400

    Fix bug related to the installation script and the config variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .models.models import db, migrate
 from .routes.routes import api_blueprint
+from config import config_map
 
 def create_app(app_env):
 
     app = Flask(__name__)
+    config = config_map.get(app_env, config_map["production"])
     app.config.from_object(app_env[1])
     app.register_blueprint(api_blueprint)
     db.init_app(app)
--- run.py
+++ run.py
@@ -5,9 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 env = os.getenv('FLASK_ENV')
-app_env = config_map.get(env, config_map['production'])
-
-app = create_app(app_env)
+app = create_app(env)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
